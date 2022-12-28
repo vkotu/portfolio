d0a65f676206dde13184b7f16232ffcce2455c7a
vkotu <vkotu@paypal.com>
2022-12-28 08:50:37 -0800

    add vercel

--- api/index.js
+++ api/index.js
@@ -4,13 +4,13 @@ import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom/server";
 import fs from "fs";
 import App from "../src/App";
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 const html = fs.readFileSync("dist/frontend/index.html").toString();
 
 const parts = html.split("Please wait");
 
-const app = express(PORT);
+// const app = express(PORT);
 
 app.use("/frontend", express.static("dist/frontend"));
 app.use(express.static("public"));
@@ -21,30 +21,19 @@ app.use("/", (req, res) => {
     </StaticRouter>
   );
   res.send(parts[0] + renderToString(reactMarkup) + parts[1]);
+  // stream
+  // res.write(parts[0]);
+  // const reactMarkup = (
+  //     <StaticRouter location={req.url}>
+  //         <App />
+  //     </StaticRouter>
+  // )
+  // const stream = renderToNodeStream(reactMarkup);
+  // stream.pipe(res, {end: false});
+  // stream.on("end", () => {
+  //     res.write(parts[1]);
+  //     res.end();
+  // })
 });
 
-app.listen(PORT, (err) => {
-  if (err) {
-    console.error(err.message);
-  } else {
-    console.log(
-      `[%s] Listening on http://localhost:%d`,
-      app.settings.env.toUpperCase(),
-      PORT
-    );
-  }
-});
-
-// stream
-// res.write(parts[0]);
-// const reactMarkup = (
-//     <StaticRouter location={req.url}>
-//         <App />
-//     </StaticRouter>
-// )
-// const stream = renderToNodeStream(reactMarkup);
-// stream.pipe(res, {end: false});
-// stream.on("end", () => {
-//     res.write(parts[1]);
-//     res.end();
-// })
+module.exports = app;
